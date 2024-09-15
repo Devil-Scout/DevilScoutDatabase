6565ff5af04bee2da5bdc60996f5f6f012cf5c01
Alexander Bhalla <xander.bhalla@gmail.com>
2024-09-15 12:49:27 -0400

    Allow authenticated to check RLS functions

--- supabase/migrations/80_security_functions.sql
+++ supabase/migrations/80_security_functions.sql
@@ -7,7 +7,7 @@ RETURNS NULL ON NULL INPUT
 RETURN permission_type IN (
   SELECT type FROM permissions WHERE user_id = (SELECT auth.uid())
 );
-REVOKE EXECUTE ON FUNCTION user_has_permission FROM public, anon, authenticated;
+REVOKE EXECUTE ON FUNCTION user_has_permission FROM public, anon;
 
 CREATE OR REPLACE FUNCTION
 user_team_num()
@@ -16,7 +16,7 @@ LANGUAGE SQL
 STABLE
 RETURNS NULL ON NULL INPUT
 RETURN (SELECT team_num FROM team_users WHERE user_id = (SELECT auth.uid()));
-REVOKE EXECUTE ON FUNCTION user_team_num FROM public, anon, authenticated;
+REVOKE EXECUTE ON FUNCTION user_team_num FROM public, anon;
 
 CREATE OR REPLACE FUNCTION
 user_on_team(team_num smallint)
@@ -25,7 +25,7 @@ LANGUAGE SQL
 STABLE
 RETURNS NULL ON NULL INPUT
 RETURN (SELECT user_team_num()) = team_num;
-REVOKE EXECUTE ON FUNCTION user_on_team FROM public, anon, authenticated;
+REVOKE EXECUTE ON FUNCTION user_on_team FROM public, anon;
 
 CREATE OR REPLACE FUNCTION
 user_on_same_team(user_id uuid)
@@ -36,4 +36,4 @@ RETURNS NULL ON NULL INPUT
 RETURN user_id IN (
   SELECT user_id FROM team_users WHERE team_num = (SELECT user_team_num())
 );
-REVOKE EXECUTE ON FUNCTION user_on_same_team FROM public, anon, authenticated;
+REVOKE EXECUTE ON FUNCTION user_on_same_team FROM public, anon;
