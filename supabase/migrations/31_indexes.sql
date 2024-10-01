afa9438b1f14d544a7da9cfa2065158e7d29879f
Alexander Bhalla <xander.bhalla@gmail.com>
2024-10-01 14:20:43 -0400

    Add indexes on submission_data for querying

---
+++ supabase/migrations/31_indexes.sql
@@ -0,0 +1,18 @@
+CREATE INDEX
+ON submission_data
+  (question_id, value_bool)
+WHERE
+  value_bool IS NOT NULL;
+
+CREATE INDEX
+ON submission_data
+  (question_id, value_int)
+WHERE
+  value_int IS NOT NULL;
+
+CREATE INDEX
+ON submission_data
+USING GIN
+  (question_id, value_text)
+WHERE
+  value_text IS NOT NULL;
