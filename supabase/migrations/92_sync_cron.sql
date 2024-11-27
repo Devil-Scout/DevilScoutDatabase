2ecb38069ae11c0fad31df139170825d40eec557
Alexander Bhalla <xander.bhalla@gmail.com>
2024-11-27 18:17:42 -0500

    Create cron sync procedures for testing

--- supabase/migrations/92_sync_cron.sql
+++ supabase/migrations/92_sync_cron.sql
@@ -8,23 +8,28 @@ GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA cron TO postgres;
 --    - International competitions will likely see slightly degraded performance
 -- - Use ETag and If-None-Match headers to reduce traffic
 
-SELECT cron.schedule(
-  job_name := 'sync-rapid',
-  schedule := '*/5 * * * *',
-  command :=
-  $$
+CREATE PROCEDURE sync.current_dblink()
+LANGUAGE sql
+AS $$
   SELECT sync.connect();
   SELECT sync.exec('CALL sync.event_teams(sync.current_event_keys())');
   SELECT sync.exec('CALL sync.event_rankings(sync.current_event_keys())');
   SELECT sync.exec('CALL sync.matches(sync.current_event_keys())');
   SELECT sync.disconnect();
+$$;
+
+SELECT cron.schedule(
+  job_name := 'sync-current',
+  schedule := '*/5 * * * *',
+  command :=
+  $$
+  CALL sync.current_dblink();
   $$
 );
 
-SELECT cron.schedule (
-  'sync-season',
-  '0 * * * *',
-  $$
+CREATE PROCEDURE sync.season_dblink()
+LANGUAGE sql
+AS $$
   SELECT sync.connect();
   SELECT sync.exec('CALL sync.districts(sync.current_year())');
   SELECT sync.exec('CALL sync.events(sync.current_year())');
@@ -33,22 +38,33 @@ SELECT cron.schedule (
   SELECT sync.exec('CALL sync.event_rankings(sync.non_current_event_keys())');
   SELECT sync.exec('CALL sync.matches(sync.non_current_event_keys())');
   SELECT sync.disconnect();
+$$;
+
+SELECT cron.schedule (
+  'sync-season',
+  '0 * * * *',
+  $$
+  CALL sync.season_dblink();
   $$
 );
+
+CREATE PROCEDURE sync.archive_dblink()
+LANGUAGE sql
+AS $$
+  SELECT sync.connect();
+  SELECT sync.exec('CALL sync.old_districts()');
+  SELECT sync.exec('CALL sync.old_events()');
+  SELECT sync.exec('CALL sync.old_event_teams()');
+  SELECT sync.exec('CALL sync.old_event_rankings()');
+  SELECT sync.exec('CALL sync.old_matches()');
+  SELECT sync.disconnect();
+$$;
 
 SELECT cron.schedule (
   'sync-archive',
   '0 6 * * 1',
   $$
-  SELECT sync.connect();
-  FOR year IN sync.old_seasons() LOOP\
-    SELECT sync.exec('CALL sync.districts(' || year || '::smallint)');
-    SELECT sync.exec('CALL sync.events(' || year || '::smallint)');
-  END LOOP;
-  SELECT sync.exec('CALL sync.event_teams(sync.old_event_keys())');
-  -- SELECT sync.exec('CALL sync.event_rankings(sync.old_event_keys())');
-  SELECT sync.exec('CALL sync.matches(sync.old_event_keys())');
-  SELECT sync.disconnect();
+  CALL sync.archive_dblink();
   $$
 );
 
