1e22a3c56a32365f357957b640e69985d0f9ec96
Alexander Bhalla <xander.bhalla@gmail.com>
2025-02-17 14:24:04 -0500

    Add team request to jwt

--- supabase/migrations/50_triggers_auth.sql
+++ supabase/migrations/50_triggers_auth.sql
@@ -8,11 +8,12 @@ AS $$
 DECLARE
   claims jsonb;
   user_team_num smallint;
+  user_requested_team_num smallint;
   user_permissions public.permission_type[];
 BEGIN
   claims := event->'claims';
 
-  -- team number
+  -- team
   SELECT team_num INTO user_team_num FROM public.team_users
     WHERE user_id = (event->>'user_id')::uuid;
 
@@ -21,6 +22,19 @@ BEGIN
     claims := jsonb_set(claims, '{team_name}', to_jsonb(
       (
         SELECT name FROM public.teams t WHERE t.number = user_team_num
+      )::text
+    ));
+  END IF;
+
+  -- team request
+  SELECT team_num INTO user_requested_team_num FROM public.team_requests
+    WHERE user_id = (event->>'user_id')::uuid;
+
+  IF user_requested_team_num IS NOT NULL THEN
+    claims := jsonb_set(claims, '{requested_team_num}', to_jsonb(user_requested_team_num));
+    claims := jsonb_set(claims, '{requested_team_name}', to_jsonb(
+      (
+        SELECT name FROM public.teams t WHERE t.number = user_requested_team_num
       )::text
     ));
   END IF;
@@ -53,6 +67,10 @@ GRANT SELECT
   TO supabase_auth_admin;
 
 GRANT SELECT
+  ON TABLE public.team_requests
+  TO supabase_auth_admin;
+
+GRANT SELECT
   ON TABLE public.permissions
   TO supabase_auth_admin;
 
@@ -61,6 +79,10 @@ CREATE POLICY "Supabase Auth can read team names" ON public.teams
   USING (true);
 
 CREATE POLICY "Supabase Auth can read team numbers" ON public.team_users
+  FOR SELECT TO supabase_auth_admin
+  USING (true);
+
+CREATE POLICY "Supabase Auth can read team requests" ON public.team_requests
   FOR SELECT TO supabase_auth_admin
   USING (true);
 
